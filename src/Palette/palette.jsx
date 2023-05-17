9be7e78db4686d43dc611ed8e3d959383e8ba511
Michaela Trčková <misa.trckova@volny.cz>
2023-05-17 15:07:18 +0200

    edit needed

---
+++ src/Palette/palette.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './palette.css';
+import SchemeColor from '../SchemeColor/schemecolor';
+
+const Palette = ({ paletteData }) => {
+  <div className="palette">
+    <div
+      className={
+        (paletteData.direction = 'vertical'
+          ? 'palette-scheme palette-scheme--vertical'
+          : 'palette-scheme palette-scheme--horizontal')
+      }
+    >
+      <img
+        className="scheme-image"
+        src={paletteData.image}
+        alt={paletteData.name}
+      />
+      <div className="scheme-colors">
+        {paletteData.colors.map((color) => (
+          <SchemeColor key={color} color={color} />
+        ))}
+      </div>
+    </div>
+    <div className="palette-info">
+      <h2>{paletteData.name}</h2>
+      <p>{paletteData.description}</p>
+
+      <p>
+        Photo by{' '}
+        <a href={paletteData.attribution.url} target="_blank">
+          {paletteData.attribution.name}
+        </a>
+        .
+      </p>
+    </div>
+  </div>;
+};
+
+export default Palette;
